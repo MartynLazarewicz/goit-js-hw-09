1c44937a438033b563d9c42f23be66959bd7827f
Martyn Lazarewicz <mk.lazarewicz@gmail.com>
2022-06-03 12:04:03 +0200

    fixed first delay in task 1 color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,24 +1,32 @@
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 const body = document.querySelector('body');
-let timerId = null;
 
 stopBtn.disabled = true;
+
+function noDelaySetInterval(func, interval) {
+  func();
+  return setInterval(func, interval);
+}
+
+function startSetInterval() {
+  interval = noDelaySetInterval(function changeColors() {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
 
 startBtn.addEventListener('click', () => {
   if ((startBtn.disabled = true)) {
     stopBtn.disabled = false;
   }
-  timerId = setInterval(() => {
-    body.style.backgroundColor = getRandomHexColor();
-  }, 1000);
+  startSetInterval();
 });
 
 stopBtn.addEventListener('click', () => {
   if ((stopBtn.disabled = true)) {
     startBtn.disabled = false;
   }
-  clearInterval(timerId);
+  clearInterval(interval);
 });
 
 function getRandomHexColor() {
