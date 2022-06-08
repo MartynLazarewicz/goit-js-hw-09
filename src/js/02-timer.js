930d09cfa4a224e2a4aa6f2c7df9d8df1629b33e
Martyn Lazarewicz <mk.lazarewicz@gmail.com>
2022-06-08 14:32:02 +0200

    Fix clearInterval on change to  wrong date

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -17,7 +17,7 @@ let timer = null;
 startBtn.disabled = true;
 
 // flatpickr library
-flatpickr(datePicker, {
+flatpickr('#datetime-picker', {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
@@ -29,45 +29,51 @@ flatpickr(datePicker, {
     if (selectedDate <= currentDate) {
       Notiflix.Notify.failure('Please choose a date in the future');
       startBtn.disabled = true;
+    } else {
+      startBtn.disabled = false;
+    }
+  },
+});
+
+function convertMs(ms) {
+  counterDown = setInterval(() => {
+    const currentDate = new Date().getTime();
+
+    const datePickerMs = new Date(
+      datePicker.value.replace(/-/g, '/')
+    ).getTime();
+    ms = datePickerMs - currentDate;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    //add leading zero
+    const addLeadingZero = value => value.toString().padStart(2, '0');
+    dataDays.innerHTML = addLeadingZero(days);
+    dataHours.innerHTML = addLeadingZero(hours);
+    dataMinutes.innerHTML = addLeadingZero(minutes);
+    dataSeconds.innerHTML = addLeadingZero(seconds);
+
+    startBtn.disabled = true;
+    if (ms <= 0) {
       clearInterval(counterDown);
       dataDays.innerHTML = '00';
       dataHours.innerHTML = '00';
       dataMinutes.innerHTML = '00';
       dataSeconds.innerHTML = '00';
-    } else {
-      startBtn.disabled = false;
-
-      function convertMs(ms) {
-        counterDown = setInterval(() => {
-          const currentDate = new Date().getTime();
-          ms = selectedDate - currentDate;
-
-          const second = 1000;
-          const minute = second * 60;
-          const hour = minute * 60;
-          const day = hour * 24;
-
-          // Remaining days
-          const days = Math.floor(ms / day);
-          // Remaining hours
-          const hours = Math.floor((ms % day) / hour);
-          // Remaining minutes
-          const minutes = Math.floor(((ms % day) % hour) / minute);
-          // Remaining seconds
-          const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-          //add leading zero
-          const addLeadingZero = value => value.toString().padStart(2, '0');
-          dataDays.innerHTML = addLeadingZero(days);
-          dataHours.innerHTML = addLeadingZero(hours);
-          dataMinutes.innerHTML = addLeadingZero(minutes);
-          dataSeconds.innerHTML = addLeadingZero(seconds);
-
-          startBtn.disabled = true;
-        }, 1000);
-      }
-
-      startBtn.addEventListener('click', convertMs);
     }
-  },
-});
+  }, 1000);
+}
+
+startBtn.addEventListener('click', convertMs);
